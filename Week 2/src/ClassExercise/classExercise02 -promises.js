06529ece90e939442fc18667a2adf9b5eca0526d
Yelong Hartl-He <Yelong.he@hotmail.com>
2023-02-08 19:21:32 +0100

    added assignment text

--- Week 2/src/ClassExercise/classExercise02 -promises.js
+++ Week 2/src/ClassExercise/classExercise02 -promises.js
@@ -1,3 +1,8 @@
+//Exercise 2
+//Create a new folder called promises and create a new file called promises.js
+//Change the calculate function to return a promise instead of a value and call the appropriate resolve or reject function depending on the result of the operation function call. Make suke to reject the promise if the operation function throws an error like divide by zero
+//Now try to chain add, substract, divide and miltiply using the .then syntax to the new calculator functions that returns a promise.
+//Change the calculator to use async/await syntax instead of promises.
 
 //Using promises
 /* const calculate = (x, y, operation) => {
@@ -10,7 +15,6 @@
     }
   });
 }; */
-
 
 // using Async/Await
 const calculate = async (x, y, operation) => {
@@ -56,5 +60,3 @@ calculate(5, 4, division)
 calculate(5, 4, substraction)
   .then((result) => console.log(result))
   .catch((error) => console.log(error));
-
-
