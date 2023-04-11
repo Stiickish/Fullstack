896e702569f2025d37ef1e16671224bc7fe0c504
Yelong Hartl-He <Yelong.he@hotmail.com>
2023-04-11 14:44:34 +0200

    Casper fik det til at virke :-) --> Se server

--- Week_9/graphql-exercise/server.ts
+++ Week_9/graphql-exercise/server.ts
@@ -2,11 +2,9 @@ import app from './app';
 import * as dotenv from 'dotenv';
 dotenv.config({ path: './config.env' });
 import mongoose from 'mongoose';
-
-export const port = process.env.PORT;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+import { ApolloServer } from 'apollo-server-express';
+import { resolvers } from './src/resolvers';
+import { typeDefs } from './src/schema';
 
 const DB = process.env.DATABASE_DEV!.replace(
   '<PASSWORD>',
@@ -14,3 +12,17 @@ const DB = process.env.DATABASE_DEV!.replace(
 );
 
 mongoose.connect(DB, {}).then(() => console.log('DB connection succesfull!'));
+
+const serverStart = async (server: ApolloServer) => {
+  await server.start();
+  server.applyMiddleware({ app });
+};
+
+const port = process.env.PORT;
+const server = new ApolloServer({ typeDefs, resolvers });
+serverStart(server);
+
+//export const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
